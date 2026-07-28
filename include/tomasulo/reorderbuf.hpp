05885c0a6de393cd25d47db8fa0ac8492fd50e93
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-07-28 11:56:20 +0800

    feat: reorder buffer implementation

---
+++ include/tomasulo/reorderbuf.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "../decoder/instr.hpp"
+
+#include <cstdint>
+#include <array>
+
+// Reorder Buffer Entry
+struct ROBEntry {
+    // is the entry occupied
+    bool busy;
+    // the instruction type
+    InstrType type;
+    // the destination arch register
+    // for arith, load, JAL/JALR/AUIPC/LUI
+    uint8_t dest_reg;
+    // the calculation result
+    uint32_t val;
+    // is the result ready
+    bool ready;
+    // the instruction's program counter
+    // for restoring and debugging
+    uint32_t pc;
+
+    // is the instruction a branch
+    bool is_branch;
+    // the predicted direction
+    // true = taken
+    bool branch_pred_taken;
+    // the actual direction taken
+    bool branch_actual_taken;
+    // the correct target address
+    uint32_t branch_target;
+
+    // is the instruction a store
+    bool is_store;
+    // the storing address
+    uint32_t store_addr;
+    // the value to store
+    uint32_t store_value;
+
+    // the tag of the entry
+    uint32_t tag;
+};
+
+// Reorder Buffer
+class ReorderBuf {
+    // ROB size
+    constexpr static size_t ROB_SIZE = 32;
+    // the current rob
+    std::array<ROBEntry, ROB_SIZE> rob;
+    // the rob in the next cycle
+    std::array<ROBEntry, ROB_SIZE> next_rob;
+    // the head and tail ptr
+    uint32_t head = 0, tail = 0;
+    // the head and tail ptr of next
+    uint32_t head_next = 0, tail_next = 0;
+    // the next tag to be allocated
+    uint32_t next_tag = 1;
+
+    // note. this cycle queue has NO unused slots
+    // empty: head == tail, head.busy == false;
+    // full:  head == tail, head.busy == true.
+
+public:
+    // --- interface for Issue phase ---
+    // try allocate one ROB entry
+    // returns tag( = tail) if success, else -1
+    int alloc(InstrType type, uint8_t dest_reg, uint32_t pc,
+        bool is_branch, bool branch_pred_taken,
+        uint32_t branch_target, bool is_store);
+    // is the current queue full
+    bool is_full() const;
+    // is the next queue full
+    bool is_next_full() const;
+
+    // --- interface for WriteBack phase ---
+    // write the result into ROB after executing
+    void write_result(uint32_t tag, uint32_t val);
+    // write branch result
+    void write_branch_result(uint32_t tag, bool taken,
+        uint32_t target);
+    // write store result
+    void write_store_result(uint32_t tag, uint32_t addr,
+        uint32_t val);
+
+    // --- interface for Commit phase ---
+    // check if head can commit
+    bool can_commit() const;
+    // return head
+    const ROBEntry& head_entry() const;
+    // confirm commit
+    void commit();
+
+    // --- mistaken prediction & restore ---
+    // flush all entries in branch_tag+1..=tail-1, clear tail
+    void flush_from_next(uint32_t branch_tag);
+
+    // --- update ---
+    // calculate next state
+    void compute_next();
+    // flip curr and next
+    void update();
+
+};
---
+++ src/tomasulo/reorderbuf.cpp
@@ -0,0 +1,161 @@
+#include "../../include/tomasulo/reorderbuf.hpp"
+
+#include <cassert>
+
+int ReorderBuf::alloc(InstrType type, uint8_t dest_reg,
+        uint32_t pc, bool is_branch, bool branch_pred_taken,
+        uint32_t branch_target, bool is_store) {
+    if (is_next_full()) {
+        return -1;
+    }
+
+    uint32_t tag = next_tag++;
+
+    ROBEntry entry = {
+        true,
+        type,
+        dest_reg,
+        0,
+        false,
+        pc,
+        is_branch,
+        branch_pred_taken,
+        false,
+        branch_target,
+        is_store,
+        0,
+        0,
+        tag
+    };
+
+    next_rob[tail_next] = entry;
+    tail_next = (tail_next + 1) % ROB_SIZE;
+    return tag;
+}
+
+bool ReorderBuf::is_full() const {
+    return head == tail && rob[head].busy == true;
+}
+
+bool ReorderBuf::is_next_full() const {
+    return head_next == tail_next
+        && next_rob[head_next].busy == true;
+}
+
+void ReorderBuf::write_result(uint32_t tag, uint32_t val) {
+    size_t index = ROB_SIZE;
+    for (size_t i = 0; i < ROB_SIZE; i++) {
+        if (rob[i].busy && rob[i].tag == tag) {
+            index = i;
+            break;
+        }
+    }
+    
+    assert(index != ROB_SIZE);
+    
+    next_rob[index].ready = true;
+    next_rob[index].val = val;
+}
+
+void ReorderBuf::write_branch_result(uint32_t tag, bool taken,
+        uint32_t target) {
+    size_t index = ROB_SIZE;
+    for (size_t i = 0; i < ROB_SIZE; i++) {
+        if (rob[i].busy && rob[i].tag == tag) {
+            index = i;
+            break;
+        }
+    }
+    
+    assert(index != ROB_SIZE);
+    assert(rob[index].is_branch == true);
+
+    next_rob[index].ready = true;
+    next_rob[index].branch_actual_taken = taken;
+    next_rob[index].branch_target = target;
+}
+
+void ReorderBuf::write_store_result(uint32_t tag, uint32_t addr,
+        uint32_t val) {
+    size_t index = ROB_SIZE;
+    for (size_t i = 0; i < ROB_SIZE; i++) {
+        if (rob[i].busy && rob[i].tag == tag) {
+            index = i;
+            break;
+        }
+    }
+    
+    assert(index != ROB_SIZE);
+    assert(rob[index].is_store == true);
+
+    next_rob[index].ready = true;
+    next_rob[index].store_addr = addr;
+    next_rob[index].store_value = val;
+}
+
+bool ReorderBuf::can_commit() const {
+    return rob[head].busy && rob[head].ready;
+}
+
+const ROBEntry& ReorderBuf::head_entry() const {
+    assert(can_commit());
+    return rob[head];
+}
+
+void ReorderBuf::commit() {
+    assert(can_commit());
+    ROBEntry& entry = rob[head];
+
+    // write arch reg (JAL/JALR are both branch and have dest_reg)
+    if (!entry.is_store && entry.dest_reg != 0) {
+        // TODO: write back the value to the regfile
+    }
+
+    // handle branch mispredict
+    if (entry.is_branch) {
+        if (entry.branch_actual_taken != entry.branch_pred_taken) {
+            // flush all entries after this branch
+            flush_from_next(entry.tag);
+            // redirect pc to this instruction's correct target
+            // TODO
+        }
+    }
+
+    // free the entry in both state and next
+    rob[head].busy = false;
+    next_rob[head].busy = false;
+    head = (head + 1) % ROB_SIZE;
+    head_next = head;
+}
+
+void ReorderBuf::flush_from_next(uint32_t branch_tag) {
+    size_t index = ROB_SIZE;
+    for (size_t i = 0; i < ROB_SIZE; i++) {
+        if (rob[i].busy && rob[i].tag == branch_tag) {
+            index = i;
+            break;
+        }
+    }
+
+    assert(index != ROB_SIZE);
+    assert(rob[index].is_branch == true);
+
+    for (size_t i = (index + 1) % ROB_SIZE; i != tail; i = (i + 1) % ROB_SIZE) {
+        next_rob[i].busy = false;
+    }
+    tail_next = (index + 1) % ROB_SIZE;
+}
+
+void ReorderBuf::compute_next() {
+    for (int i = 0; i < ROB_SIZE; i++) {
+        next_rob[i] = rob[i];
+    }
+    head_next = head;
+    tail_next = tail;
+}
+
+void ReorderBuf::update() {
+    std::swap(rob, next_rob);
+    std::swap(head, head_next);
+    std::swap(tail, tail_next);
+}
