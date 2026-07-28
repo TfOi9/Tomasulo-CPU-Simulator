afcc5f50124dea9ae0f29f10c3d5bd2714f65392
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-07-28 09:38:07 +0800

    feat: arch reg file

---
+++ include/tomasulo/regfile.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+#include <string>
+
+// Architecture Register File
+// keeps two groups of physical registers
+class ArchRegFile {
+    // file size
+    constexpr static size_t FILE_SIZE = 32;
+    // current state registers
+    std::array<uint32_t, FILE_SIZE> regs;
+    // next state registers
+    std::array<uint32_t, FILE_SIZE> new_regs;
+
+public:
+    // flush zero into x0 in next
+    void flush_zero();
+    // read register data from current state
+    uint32_t read_reg(size_t index) const;
+    // write the value into a next state register
+    void write_reg(size_t index, uint32_t val);
+    // update the state, flipping curr and next
+    void update();
+    // dump all the registers for debugging
+    std::string dump() const;
+
+};
+
+const std::string reg_indices[] = {
+    "x0", "x1", "x2", "x3",
+    "x4", "x5", "x6", "x7",
+    "x8", "x9", "x10", "x11",
+    "x12", "x13", "x14", "x15",
+    "x16", "x17", "x18", "x19",
+    "x20", "x21", "x22", "x23",
+    "x24", "x25", "x26", "x27",
+    "x28", "x29", "x30", "x31",
+};
+
+const std::string reg_names[] = {
+    "zero", "ra", "sp", "gp",
+    "tp", "t0", "t1", "t2",
+    "s0", "s1", "a0", "a1",
+    "a2", "a3", "a4", "a5",
+    "a6", "a7", "s2", "s3",
+    "s4", "s5", "s6", "s7",
+    "s8", "s9", "s10", "s11",
+    "t3", "t4", "t5", "t6",
+};
---
+++ src/tomasulo/regfile.cpp
@@ -0,0 +1,42 @@
+#include "../../include/tomasulo/regfile.hpp"
+
+#include <cassert>
+#include <ios>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+void ArchRegFile::flush_zero() {
+    new_regs[0] = 0;
+}
+
+uint32_t ArchRegFile::read_reg(size_t index) const {
+    assert(index >= 0 && index < FILE_SIZE);
+    return regs[index];
+}
+
+void ArchRegFile::write_reg(size_t index, uint32_t val) {
+    assert(index >= 0 && index < FILE_SIZE);
+    new_regs[index] = val;
+}
+
+void ArchRegFile::update() {
+    std::swap(regs, new_regs);
+}
+
+std::string ArchRegFile::dump() const {
+    std::stringstream ss;
+
+    ss << std::setw(4) << "#"
+       << std::setw(5) << "name"
+       << std::setw(9) << "value"
+       << std::endl;
+    for (size_t i = 0; i < FILE_SIZE; i++) {
+        ss << std::setw(4) << reg_indices[i].c_str()
+           << std::setw(5) << reg_names[i].c_str()
+           << std::setw(9) << std::uppercase << std::hex << regs[i]
+           << std::endl;
+    }
+
+    return ss.str();
+}
