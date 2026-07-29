2fd1106aa53410c6cc33885574066b10a99769a6
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-07-29 09:55:10 +0800

    fix: naive branch pred logic fix: pc + 4

--- src/tomasulo/branchpred.cpp
+++ src/tomasulo/branchpred.cpp
@@ -1,7 +1,7 @@
 #include "../../include/tomasulo/branchpred.hpp"
 
 std::pair<bool, uint32_t> NaivePredictor::predict(uint32_t pc, int32_t imm) {
-    return std::make_pair(false, pc);
+    return std::make_pair(false, pc + 4);
 }
 
 void NaivePredictor::update(uint32_t pc, bool actual_taken) {}
